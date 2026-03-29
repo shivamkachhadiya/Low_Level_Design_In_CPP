3dd6b5d97adf3a5769d0a8cba4ed608f99db7ed0
shivamkachhadiya <shivamkachhadiya68@gmail.com>
2026-03-29 19:14:44 +0530

    update document rendering

--- Desgin Google Docs/src/desgin_google_docs.cpp
+++ Desgin Google Docs/src/desgin_google_docs.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "TextElement.hpp"
 #include "ImageElement.hpp"
+#include "../include/Document.hpp"
+using namespace std;
 
 int main()
 {
     // TextElement t("Hello, FAANG!");
     // std::cout << t.render() << std::endl;
-    ImageElement img("aaa.jpg");
-    std::cout << img.render() << std::endl;
+    // ImageElement img("aaa.jpg");
+    // std::cout << img.render() << std::endl;
+
+    // make Document Object
+    Document doc;
+    // add text element
+    doc.addElement(make_unique<TextElement>("Hello, FAANG:\n"));
+    // add image element
+    doc.addElement(make_unique<ImageElement>("pic.jpg\n"));
+    // render full document
+    cout << doc.getDocument() << endl;
 }
